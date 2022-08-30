b84f730463adcd87b75c868c02d8edcc610033f3
FedeVts <ramella93@gmail.com>
2022-08-29 23:55:12 -0300

    falta

--- js/segundo-complementario.js
+++ js/segundo-complementario.js
@@ -1,5 +1,5 @@
 class Cliente{
-    constructor(producto, precioProducto, cantidadDeProductos, cuotaProducto){
+    constructor(producto, precioProducto, cantidadDeProductos, cuotaProducto,){
         this.producto = producto;
         this.precioProducto = precioProducto;
         this.cantidadDeProductos = cantidadDeProductos;
@@ -11,7 +11,7 @@ class Cliente{
 
 
 let listaCliente = [];
-for(let i = 0 ; i < 3 ; i++){
+for(let i = 0 ; i < 2 ; i++){
 let producto = prompt("Ingrese el tipo de producto");
 let precioProducto = parseInt(prompt("ingrese el costo de " + producto));
 let cantidadDeProductos = parseInt(prompt("Indique la cantidad que desea compra en unidades"));
@@ -34,34 +34,38 @@ function precioEnCuotas(){
    if (cuotaProducto == 6){
        let interesesEn6Cuotas = (precioTotal(precioProducto, cantidadDeProductos) * 5) / 100;
        let precioFinal = precioTotal(precioProducto, cantidadDeProductos) + agregadoDeIva + interesesEn6Cuotas;
-      alert("El precio final iva e intereces incluidos es de/los/las" + producto + " es de $" + precioFinal + " que disfrute su compra");
-      return("Su cuota mensual es de $" + (precioFinal / cuotaProducto));
+      alert("El precio final iva e intereces incluidos es de " + producto + " es de $" + precioFinal + " que disfrute su compra");
+      return("Su cuota mensual de " + producto + " es de $ " + (precioFinal / cuotaProducto));
    }
    else if (cuotaProducto == 12){
        let interesesEn12Cuotas = (precioTotal(precioProducto, cantidadDeProductos) * 10 ) / 100; 
        let precioFinal = precioTotal(precioProducto, cantidadDeProductos) + agregadoDeIva + interesesEn12Cuotas;
-       alert("El precio final iva e intereces incluidos de/los/las " + producto + " es de $" + precioFinal + " que disfrute su compra");
-       return("Su cuota mensual es de $" + (precioFinal / cuotaProducto));
+       alert("El precio final iva e intereces incluidos de " + producto + " es de $" + precioFinal + " que disfrute su compra");
+       return("Su cuota mensual de " + producto + " es de $ " + (precioFinal / cuotaProducto));
 
    }
    else if (cuotaProducto == 18){
        
        let interesesEn18Cuotas = (precioTotal(precioProducto, cantidadDeProductos) * 15) / 100;  
        let precioFinal = precioTotal(precioProducto, cantidadDeProductos) + agregadoDeIva + interesesEn18Cuotas;
-       alert("El precio final iva e intereces incluidos de/los/las " + producto + " es de $" + precioFinal + " que disfrute su compra");
-       return("Su cuota mensual es de $" + (precioFinal / cuotaProducto));
+       alert("El precio final iva e intereces incluidos de " + producto + " es de $" + precioFinal + " que disfrute su compra");
+       return("Su cuota mensual de " + producto + " es de $ "+ (precioFinal / cuotaProducto));
    }
    else if (cuotaProducto == 24 ){ 
        let interesesEn24Cuotas = (precioTotal(precioProducto, cantidadDeProductos) * 20) / 100;
        let precioFinal = precioTotal(precioProducto, cantidadDeProductos) + agregadoDeIva + interesesEn24Cuotas;
-       alert("El precio final iva e intereces incluidos de/los " + producto + " es de $" + precioFinal + " que disfrute su compra");
-       return("Su cuota mensual es de $" + (precioFinal / cuotaProducto));
+       alert("El precio final iva e intereces incluidos de " + producto + " es de $" + precioFinal + " que disfrute su compra");
+       return("Su cuota mensual de " + producto + " es de $ " + (precioFinal / cuotaProducto));
 
    }
    else{
        alert("El texto/numero ingresado es invalido, vuelva a intentarlo")
        console.log("El texto/numero ingresado es invalido, vuelva a intentarlo");
    }
+   
+}
+console.log(precioEnCuotas());
+alert(precioEnCuotas());
 }
 for(let cliente of listaCliente){
     console.log("<--------->")
@@ -69,9 +73,9 @@ for(let cliente of listaCliente){
     console.log("Precio " , cliente.precioProducto);
     console.log("Cantidad " , cliente.cantidadDeProductos);
     console.log("Cuotas " , cliente.cuotaProducto);
-    
+    // console.log("el precio de la cuota es: " cliente.precioEnCuotas);
 }
-alert(precioEnCuotas());
-console.log(precioEnCuotas());
-}
+
+
+
 console.log(listaCliente);
